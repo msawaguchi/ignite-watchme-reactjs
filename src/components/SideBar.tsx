9338992d43ab763e35d1ab06255db473a3de9cc8
Missae Sawaguchi <missae.sawaguchi@gmail.com>
2021-06-16 23:10:23 -0300

    Create Content and Sidebar components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: Genre[];
+  selectedGenreId: number;
+  handleClickButton(id: number): void;
+}
+
+interface Genre {
+  id: number;
+  title: string;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <nav className='sidebar'>
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className='buttons-container'>
+        {props.genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
